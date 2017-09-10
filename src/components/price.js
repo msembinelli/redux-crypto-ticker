bbd62720aab1e0d5ea4c507f0e8b1f1e6e0f4bfa
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2017-09-10 11:06:30 -0600

    Add price skeleton component

---
+++ src/components/price.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Price extends Component {
+  render() {
+    return (
+      <div className="price">
+        <h3>$4000</h3>
+      </div>
+    );
+  }
+}
