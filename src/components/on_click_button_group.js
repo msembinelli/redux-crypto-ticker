003cb3ca2d260380a93e0fa2134798df1a5c263e
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2017-10-03 00:00:41 -0600

    Change buttons to grey

--- src/components/on_click_button_group.js
+++ src/components/on_click_button_group.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default (props) => {
     return (
       <div className='row justify-content-center' style={props.style}>
-        {props.buttons.map(button => { return <button key={button.name} type='button' onClick={button.func} className='btn btn-lg btn-outline-primary'>{button.name}</button> })}
+        {props.buttons.map(button => { return <button key={button.name} type='button' onClick={button.func} className='btn btn-lg btn-outline-grey'>{button.name}</button> })}
       </div>
     );
 }
